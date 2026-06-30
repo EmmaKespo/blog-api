e2d936e31bcd1245edade889a3085eea4123bb94
EmmaKespo <kespoemma@gmail.com>
2026-06-30 12:48:40 +0100

    export router

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -5,3 +5,4 @@ const commentController = require('../controllers/commentController')
 // Routes are scoped relative to a specific post
 router.get('/:postId/comments', commentController.getCommentByPost)
 router.post('/postId/comments', commentController.createComment)
+module.exports = router;
