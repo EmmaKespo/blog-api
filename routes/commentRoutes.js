e799bd432cd85df38a656b353bb1689ca80a5223
EmmaKespo <kespoemma@gmail.com>
2026-06-30 12:47:02 +0100

    created routes for grt and post to create and get comment

---
+++ routes/commentRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController')
+
+// Routes are scoped relative to a specific post
+router.get('/:postId/comments', commentController.getCommentByPost)
+router.post('/postId/comments', commentController.createComment)
