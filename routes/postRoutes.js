c04ae2a061209f0f77eab9ca0b62f6a2d9cb5989
EmmaKespo <kespoemma@gmail.com>
2026-07-01 17:10:56 +0100

    authentication for users not to perform certain action

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 const router = express.Router();
+import requireAuth from '../middleware/authMiddleware.js';
 
-// You must explicitly add the '.js' extension for local files
 import postController from '../controllers/postController.js'; 
 
 
 // public routes to view posts
-router.get('/', postController.getAllPosts); //to fetch posts
+router.get('/',  postController.getAllPosts); //to fetch posts
  // author routes to manage post
-router.post('/:id', postController.createPost); 
-router.put('/:id', postController.updatePost);
-router.patch('/:id/publish', postController.togglePublish);
-router.delete('/:id', postController.deletePost)
+router.post('/:id', requireAuth, postController.createPost); 
+router.put('/:id', requireAuth, postController.updatePost);
+router.patch('/:id/publish', requireAuth, postController.togglePublish);
+router.delete('/:id', requireAuth, postController.deletePost)
 
 export default router;
