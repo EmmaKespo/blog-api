95e7b751ece609663bab17882a24eee7147d91a1
EmmaKespo <kespoemma@gmail.com>
2026-07-02 10:40:32 +0100

    comment validation routes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // You must explicitly add the '.js' extension for local files
 import commentController from '../controllers/commentController.js'; 
+import validate from '../middleware/validateMiddleware.js';
+import { createCommentSchema } from '../validators/commentValidator.js';
 
 
 // Routes are scoped relative to a specific post
 router.get('/:postId/comments', commentController.getCommentByPost)
-router.post('/:postId/comments', commentController.createComment)
+router.post('/:postId/comments', validate(createCommentSchema), commentController.createComment)
 export default router;
